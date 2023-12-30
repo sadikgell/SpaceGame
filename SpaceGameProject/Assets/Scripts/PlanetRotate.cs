2822a2ec2d924a9223b34e42fd2b10ab537c5308
İbrahim Emre Baş <ibrahimbas1414@gmail.com>
2023-12-30 20:28:28 +0300

    Level1 üzerinde düzenlemeler yapıldı. script eklendievel 1 üzerinde denendi
    Level1 üzerinde düzenlemeler yapıldı. Gezegenlerin kendi etrafında dönmesi için script eklendi ve level1 üzerinde denendi

---
+++ SpaceGameProject/Assets/Scripts/PlanetRotate.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlanetRotate : MonoBehaviour
+{
+    public float rotateSpeed = 10f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(-Vector3.up, rotateSpeed * Time.deltaTime);
+    }
+}
